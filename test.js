d35a0fbb740caa607b5207f9ca655f3d25f77942
Angie Vanegas P <arco527@gmail.com>
2024-05-10 16:50:30 +0000

    Refact code

--- test.js
+++ test.js
@@ -15,36 +15,37 @@ test('3.5 euros should be 3.7 dollars', () => {
     const{fromEuroToDollar} = require('./app.js');
 
     // uso la función como debe ser usada
-    const euros = fromEuroToDollar(3.5)
+    const dollar = fromEuroToDollar(3.5)
 
     // si. 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-    const expected = euros * 1.07
-    console.log(euros)
-    console.log(expected)
+    // const expected = euros * 1.07
+    // console.log(euros)
+    // console.log(expected)
 
     // hago la comparación (prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(dollar).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
 
 test('5.6 dollar should be 876.4 Yenes', () => {
     const{fromDollarToYen} = require('./app.js')
 
-    const dollar = fromDollarToYen(5.6)
+    const yen = fromDollarToYen(5.6)
 
-    const expected = dollar + 156.5
-    console.log(expected)
+    // const expected = yen + 156.5
+    // console.log(expected)
 
-    expect(fromDollarToYen(5.6)).toBe(876.4)
+    expect(yen).toBe(876.4)
 })
 
 test('One Yen should be 0.87 Pounds', () => {
     const{fromYenToPound} = require('./app.js')
     
-    const yen = fromYenToPound(1)
+    const pounds = fromYenToPound(1)
+    // console.log('funcionYen', yen)
 
-    const expected = yen * 0.87
-    console.log(expected)
+    // const expected = yen * 0.87
+    // console.log('yen', expected)
 
-    expect(fromYenToPound(1)).toBe(0.87)
+    expect(pounds).toBe(0.87)
 
 })
